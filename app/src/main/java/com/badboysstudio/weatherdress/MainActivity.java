e92cd4c838f7ebf7f2829325f0a12dadda9e285e
Anthony <anthonysiu@gmail.com>
2016-09-25 18:42:29 -0700

    tab swipe, front page scrollable, but cut off

--- app/src/main/java/com/badboysstudio/weatherdress/MainActivity.java
+++ app/src/main/java/com/badboysstudio/weatherdress/MainActivity.java
@@ -1,112 +1,67 @@
 package com.badboysstudio.weatherdress;
 
-import com.badboysstudio.weatherdress.model.Weather;
-
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
 import android.widget.TextView;
-
-import org.json.JSONException;
-
-import java.util.ArrayList;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView commentText;
-    private Context context;
-    ArrayList<City> cities;
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //http://api.openweathermap.org/data/2.5/weather?q=vancouver,canada&APPID=a70c9988f0ae23aca5840d9f6d957920
-        /*
-        {"coord":{"lon":-123.12,"getWeatherDatalat":49.25},"weather":[{"id":500,"main":"Rain","description":"light rain","icon":"10d"}],"base":"stations","main":{"temp":288.9,"pressure":1028,"humidity":55,"temp_min":285.37,"temp_max":292.04},"wind":{"speed":1.54,"deg":100},"rain":{"1h":0.51},"clouds":{"all":64},"dt":1474761579,"sys":{"type":3,"id":37665,"message":0.0175,"country":"CA","sunrise":1474812258,"sunset":1474855355},"id":6173331,"name":"Vancouver","cod":200}
-         */
-        context = this;
-        ActionBar actionBar = getSupportActionBar();
-        //actionBar.setDisplayShowTitleEnabled(false);
-        //actionBar.setDisplayShowHomeEnabled(false);
-        actionBar.setTitle("123");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        /*Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);*/
-
-        commentText = (TextView)findViewById(R.id.comment);
-        commentText.setText("comment123");
-
-        //String city = "Vancouver,Canada";
-        //String city = "chicago,us";
-        CityName city = CityName.Chicago;
-        String city_string = "newyork,us";
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
 
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            JSONWeatherTask task = new JSONWeatherTask();
-            //task.execute(new String[]{city_string});
-            task.execute(CityName.Chicago);
-        } else {
-            commentText.setText("No network connection available.");
-        }
-    } // end onCreate
-
-    private class JSONWeatherTask extends AsyncTask<CityName, Void, Weather> {
-        String teststring = "hey";
-
-        @Override
-        protected Weather doInBackground(CityName... params) {
-            Weather weather = new Weather();
-            String data = ( (new WeatherHttpClient()).getWeatherData(context, params[0]));
-
-            try {
-                weather = JSONWeatherParser.getWeather(data);
-
-                // Let's retrieve the icon
-                //weather.iconData = ( (new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
-
-            } catch (JSONException e) {
-                e.printStackTrace();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-            return weather;
+        });
 
-        }
+    }
 
-        @Override
-        protected void onPostExecute(Weather weather) {
-            super.onPostExecute(weather);
-
-            String string = teststring;
-
-            /*if (weather.iconData != null && weather.iconData.length > 0) {
-                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
-                imgView.setImageBitmap(img);
-            }
-
-            cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
-            condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");*/
-            commentText.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "°C");
-
-        } // end onPostExecute
-    } // end JSONWeatherTask
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -117,14 +72,103 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            case R.id.action_home:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    } // end onOptionsItemSelected
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-} // end MainActivity
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            int section = getArguments().getInt(ARG_SECTION_NUMBER);
+            View rootView;
+            switch(section){
+                case 0:
+                    rootView = inflater.inflate(R.layout.activity_main_old, container, false);
+                    break;
+                case 1:
+                    rootView = inflater.inflate(R.layout.activity_favorite, container, false);
+                    break;
+                default:
+                    rootView = inflater.inflate(R.layout.activity_main_old, container, false);
+                    break;
+            }
+
+
+            //View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
+}
