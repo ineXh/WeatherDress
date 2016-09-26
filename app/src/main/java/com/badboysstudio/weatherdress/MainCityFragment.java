d966b87152fda80e0afff9aa6f45982d8c3f7bef
Anthony <anthonysiu@gmail.com>
2016-09-25 22:38:11 -0700

    wip

--- app/src/main/java/com/badboysstudio/weatherdress/MainCityFragment.java
+++ app/src/main/java/com/badboysstudio/weatherdress/MainCityFragment.java
@@ -16,12 +16,16 @@ import com.badboysstudio.weatherdress.model.Weather;
 
 import org.json.JSONException;
 
+import java.util.ArrayList;
+
 /**
  * Created by siuant on 9/25/2016.
  */
 public class MainCityFragment extends android.support.v4.app.Fragment {
     private TextView commentText;
     Context context;
+    ArrayList<City> cities;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -35,8 +39,14 @@ public class MainCityFragment extends android.support.v4.app.Fragment {
         super.onActivityCreated(bundle);
 
         context = getActivity();
+        View view = getView();
         commentText = (TextView)getView().findViewById(R.id.comment);
         commentText.setText("comment123");
+
+        cities = new ArrayList<City>();
+        cities.add(new City(context, view, CityName.Chicago));
+        cities.add(new City(context, view, CityName.Seattle));
+        cities.add(new City(context, view, CityName.NewYork));
 
         ConnectivityManager connMgr = (ConnectivityManager)
                 getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -44,19 +54,25 @@ public class MainCityFragment extends android.support.v4.app.Fragment {
         if (networkInfo != null && networkInfo.isConnected()) {
             JSONWeatherTask task = new JSONWeatherTask();
             //task.execute(new String[]{city_string});
-            task.execute(CityName.Chicago);
+            //task.execute(CityName.Chicago);
+            //for(City  city : cities){
+                //task.execute(city);
+            //}
+            task.execute(cities.get(0), cities.get(1));
         } else {
             commentText.setText("No network connection available.");
         }
 
     } // end onActivityCreated
 
-    private class JSONWeatherTask extends AsyncTask<CityName, Void, Weather> {
+    private class JSONWeatherTask extends AsyncTask<City, Void, Weather> {
         String teststring = "hey";
+        City city;
 
         @Override
-        protected Weather doInBackground(CityName... params) {
+        protected Weather doInBackground(City... params) {
             Weather weather = new Weather();
+            this.city = params[0];
             String data = ( (new WeatherHttpClient()).getWeatherData(context, params[0]));
 
             try {
@@ -70,7 +86,7 @@ public class MainCityFragment extends android.support.v4.app.Fragment {
             }
             return weather;
 
-        }
+        } // end doInBackground
 
         @Override
         protected void onPostExecute(Weather weather) {
@@ -85,8 +101,8 @@ public class MainCityFragment extends android.support.v4.app.Fragment {
 
             cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
             condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");*/
-            commentText.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "°C");
-
+            //commentText.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "°C");
+            city.setTemperatureText(Math.round((weather.temperature.getTemp() - 273.15)));
         } // end onPostExecute
     } // end JSONWeatherTask
 
